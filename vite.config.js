9684c6d80c19ad99299f265c4390d08243122726
Akilesh-programmer <akileshakileshs1234@gmail.com>
2025-08-25 15:24:42 +0530

    added the Local photos and json

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '10.57.1.233',
-    port: 5173, // You can change the port if needed
-  },
+  plugins: [react(),tailwindcss()],
 })
