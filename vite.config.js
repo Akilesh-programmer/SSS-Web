f65ea421152bf53c58ec587e127ecaa281454e84
vijayguhan10 <vijayguhan10@gmail.com>
2025-08-25 14:27:36 +0530

    added the web

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '10.57.1.233',
+    port: 5173, // You can change the port if needed
+  },
 })
