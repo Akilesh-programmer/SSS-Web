0347ad2108b53def7807f853253aaa0426e33ae0
Akilesh <akileshakileshs1234@gmail.com>
2025-09-05 00:46:58 +0530

    Added Image

--- src/data/departmentsData.js
+++ src/data/departmentsData.js
@@ -740,7 +740,7 @@ const departments = [
         specialization: "Medical & Cosmetic Dermatology",
         qualifications: "MD (DERM)",
         availability: "Mon-Sat: 9AM-6PM",
-        image: "/src/assets/Sanjith-SSS-Hospitals.jpg",
+        image: "/src/assets/Sanjith-SSS-Hospitals.png",
         achievements: [
           "Founder of SSS Super Speciality Hospital",
           "Expert in Advanced Dermatological Procedures",
