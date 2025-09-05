ee95a0c1eb92302cbca43ef1aa64c3f42f5df42f
Akilesh <akileshakileshs1234@gmail.com>
2025-09-06 00:23:20 +0530

    Included Patient Testimonials

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,20 @@
+export const testimonials = [
+  {
+    name: "Sathish Kumar D",
+    review:
+      "Today i have visited MHC health check up.The hospital is incredibly clean and well-maintained it feels more like a hotel than a medical facility and also the ospital’s design promotes healing calm colors, quiet hallways, and thoughtful architecture.By the way Doctors and nurses were compassionate, professional, and attentive to every detail.Receptionist was warm and welcoming she made a great first impression and Polite, professional, and calm under pressure exactly what you’d hope for at the front desk.She greeted every patient with a smile and treated everyone with respect.Over all staff and nurses were good at while handing the patient.",
+    rating: 5,
+  },
+  {
+    name: "msraja shanmugam",
+    review:
+      "First modern Hospital in erode.. fantastic infrastructure..no need to go for Coimbatore city..all facilities and latest technology available in SSS SUPER SPECIALITY HOSPITAL erode..",
+    rating: 5,
+  },
+  {
+    name: "Balachander Udhayakumar",
+    review:
+      "Pretty happy with the service overall. We did master health check up over here, and the package that they offer is quite attractive.. All the doctors are quite friendly and cooperative. Moreover, they managed to communicate the issue clearly.. They are so patient enough to listen to our health issues, after which relevant solutions / treatment have been given..Good hospitality.. Staff strength is also quite good as they allot 1 dedicated staff who is handling 4 patients at once.. A slight improvement in cafeteria menu is required though, as they options are only less..",
+    rating: 5,
+  },
+];
